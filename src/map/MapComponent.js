182b1612bb6ee1e579528b9c81c22b37b4a71102
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-08-17 00:19:10 +0200

    refac: includes the mobx and decorators

--- src/map/MapComponent.js
+++ src/map/MapComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import { observer } from "mobx-react";
+import { action, observable } from "mobx";
+import { GoogleApiWrapper, Map, Marker } from "google-maps-react";
 
 const MapContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.lightdark};
@@ -9,63 +11,51 @@ const MapContainer = styled.div`
 `;
 
 @GoogleApiWrapper({ apiKey: process.env.REACT_APP_GOOGLE_API })
+@observer
 export class MapComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: { lat: 0, lng: 0, zoom: 8 },
-      viewport: null
-    };
-  }
+  @observable
+  location = { lat: 0, lng: 0 };
+  @observable
+  viewport = null;
+
+  @action
   searchBoxHandler = () => {
     const input = document.getElementById("google-search-input");
     const searchBox = new this.props.google.maps.places.SearchBox(input);
     searchBox.addListener("places_changed", () => {
       var places = searchBox.getPlaces();
-      this.setState({
-        location: places[0].geometry.location,
-        viewport: places[0].geometry.viewport
-      });
+      (this.location = places[0].geometry.location),
+        (this.viewport = places[0].geometry.viewport);
     });
   };
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
-      this.setState({
-        location: {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-          zoom: 8
-        }
-      });
+      this.location = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
     });
   }
 
   render() {
     this.searchBoxHandler();
     const bounds = new this.props.google.maps.LatLngBounds();
-    if (!this.state.viewport) {
-      bounds.extend(this.state.location);
+    if (!this.viewport) {
+      bounds.extend(this.location);
     } else {
-      bounds.union(this.state.viewport);
+      bounds.union(this.viewport);
     }
 
     return (
       <Map
         style={{ width: "500px", height: "500px" }}
         google={this.props.google}
-        zoom={this.state.location.zoom}
+        zoom={8}
         bounds={bounds}
       >
-        <Marker position={this.state.location} />
+        <Marker position={this.location} />
       </Map>
     );
   }
 }
-
-// export class MapComponent extends Component {
-//   render() {
-//     return <Mapper />;
-//   }
-// }
